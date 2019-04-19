e54f78eb7f77afb2eda2a8c91d5da61398edf603
IslamNasr <islam.mahmoud@student.guc.edu.eg>
2019-04-19 16:51:08 +0200

    decoding class

--- Newbies_ISA_Implementation/src/stages/InstructionsDecoding.java
+++ Newbies_ISA_Implementation/src/stages/InstructionsDecoding.java
@@ -1,11 +1,13 @@
 package stages;
 
+import memory.ControlUnit;
+
 public class InstructionsDecoding {
 String ReadData1;
 String ReadData2;
 
 
-	public static void Decode(String Instruction) {
+	public static void Decode(String Instruction,ControlUnit control) {
 		
 	}
 	
